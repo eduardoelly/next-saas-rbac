0b5ce00b4fcc5c4d701ab05bcb4d7b4365bbf2ba
Eduardo Elly <eduardo_elly@hotmail.com>
2025-11-10 18:32:45 -0300

    feat: obtendo usuário autenticado

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div>Hello World</div>
+import { auth } from '@/auth/auth'
+
+export default async function Home() {
+  const { user } = await auth()
+
+  return <pre>{JSON.stringify(user, null, 2)}</pre>
 }
