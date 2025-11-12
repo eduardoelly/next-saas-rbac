64de5fc9bb18bd510e35d4718be08102865cd436
Eduardo Elly <eduardo_elly@hotmail.com>
2025-11-12 18:39:49 -0300

    fix: corrigido erro de import

--- apps/web/src/app/auth/sign-up/sign-up-form.tsx
+++ apps/web/src/app/auth/sign-up/sign-up-form.tsx
@@ -100,11 +100,6 @@ export function SignUpForm() {
         </Button>
 
         <Separator />
-
-        <Button type='submit' className='w-full' variant='outline'>
-          <Image src={githubIcon} alt='' className='mr-2 size-4 dark:invert' />
-          Sign up with GitHub
-        </Button>
       </form>
 
       <Separator />
